a652c418a7f5ba6e671e14dfa3387efc77b01455
KASSY <133909539+DollyWhite06@users.noreply.github.com>
2025-01-21 16:27:59 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { SolarisComponent } from './solaris/solaris.component';
+import { GranosSolarisComponent } from './granos-solaris/granos-solaris.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: 'home', component: HomeComponent},
+{path: 'contacto', component: ContactoComponent},
+{path: 'Solaris', component: SolarisComponent},
+{path: 'GranosSolaris', component: GranosSolarisComponent}
+
+
+];
